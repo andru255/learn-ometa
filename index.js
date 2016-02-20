26df365e1d3605f27397cc6774423fcecd222b0b
andru255 <andru255@gmail.com>
2016-02-20 12:47:45 -0500

    first single demo

---
+++ index.js
@@ -0,0 +1,14 @@
+var fs    = require("fs");
+
+var ohm = require("ohm-js");
+var grammarString = fs.readFileSync('grammar.ohm');
+var grammarObj = ohm.grammar(grammarString);
+
+var testFile = fs.readFileSync('test.txt');
+var meta = grammarObj.match(testFile.toString());
+
+if(meta.succeeded()){
+  console.log("cool!");
+} else {
+  console.log(meta.message);
+}
