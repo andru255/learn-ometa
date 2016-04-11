408691f396962f62934906cf54079c214960d679
andru255 <andru255@gmail.com>
2016-04-10 21:26:07 -0500

    Created Only tags width no content

--- tag-script/index.js
+++ tag-script/index.js
@@ -9,23 +9,27 @@ var meta = grammarObj.match(testFile.toString());
 var semantics = grammarObj.semantics();
 
 semantics.addOperation('value', {
-    "tags": function(line){
-        return line.value();
+    "tags": function(line, space, next, eol){
+        console.log("next!", next.value());
+        return [ line.value()].concat(  next.value() );
     },
-    "row": function(initialSpace, tagName, posibleText, eol){
+    "row": function(initialSpace, tagName){
         var indent = initialSpace.value()[0].length;
-        return [ indent, tagName.value(), posibleText.value()];
+        return [ indent, tagName.value()];
     },
     "pairWithTag": function(space, text){
+        console.log("pairWithTag:space Found", space.value());
+        console.log("pairWithTag:text Found", text.value());
         return [space.value(), text.value()];
     },
+    "textInline": function(text, follow, eol){
+        var inline = [ text.value() ].concat(follow.value());
+        return [inline];
+    },
     "closingTags": function(line){
-        console.log("closingTags", line.value());
+        console.log("closingTags:Tag Found", line.value());
         return [ line.value()];
     },
-    //"startWithWSLine": function(ws, tagName, text){
-    //    return [ ws.value(), tagName.value(), text.value()];
-    //},
     "space": function(space){
         return "SPC";
     }
