13371f46718bc0c80338f89111e08e28c4467b93
andru255 <andru255@gmail.com>
2016-04-17 10:37:33 -0500

    feat(CommentSript): Refactoring the AST output

--- comment-script/index.js
+++ comment-script/index.js
@@ -8,24 +8,39 @@ var testFile = fs.readFileSync('test-script.txt').toString();
 var meta = grammarObj.match(testFile.toString());
 var semantics = grammarObj.semantics();
 
-semantics.addOperation('interpret', {
-    "Comments": function(e){
-        console.log("e", e.interpret());
-        return e.interpret();
+semantics.addOperation('value', {
+    "comments": function(line, space, next, eol){
+        var tree = [ line.value()].concat(  next.value() );
+        return tree;
     },
-    "singleLineComment": function(x, _, y){
-        //console.log("singleLineComment_singleLine:args", arguments);
-        return this.interval.contents;
+    "comment": function(initialSpace, text){
+        var indent = initialSpace.value()[0].length;
+        return {
+            indent : indent,
+            content: text.value()
+        };
     },
-    "multiLineComment": function(x, _, y){
-        //console.log("multiLineComment_multiLine:args", arguments);
-        return this.interval.contents;
+    "singleLineComment": function(startComment, content){
+        var contentInComment = content.value().join("");
+        return {
+            type        : "singleComment",
+            startComment: startComment.value(),
+            content     : contentInComment
+        };
+    },
+    "multiLineComment": function(startComment, content, endComment){
+        var contentInComment = content.value().join("");
+        return{
+            type        : "MultilineComment",
+            startComment: startComment.value(),
+            content     : contentInComment
+        };
     }
 });
 
 if(meta.succeeded()){
   console.log("cool! it's time to know, ¿Whats up here?");
-  semantics(meta).interpret();
+  console.log(semantics(meta).value());
 } else {
   console.log(meta.message);
 }
